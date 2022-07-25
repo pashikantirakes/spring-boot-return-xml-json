29a8770b1d3ec9d969a970e349800225505ba072
pashikantirakes <94496861+pashikantirakes@users.noreply.github.com>
2022-07-25 15:09:52 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,28 @@
+package in.java.com;
+
+
+public class A {
+     private String name;
+     private int id;
+	public A(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "A [name=" + name + ", id=" + id + "]";
+	}
+}
